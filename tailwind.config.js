e1344f5e6c0785f2d3db03115b27207d310a4b38
Moynul Islam <moynul.officials@gmail.com>
2023-05-29 02:17:22 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    colors: {
+      'image_bg': '#ccc',
+      'body_color': '#3c3c43',
+      'nav_bg': '#232229',
+      'side_nav': '#35353c',
+      'text-color': '#fff',
+      'txt_hover': '#85848B'
+    },
+    fontSize: {
+      'xsm_text': '12px',
+      'sm_text': '14px',
+      'sm_icon': '8px',
+      'md_icon': '20px',
+      'lg_icon': '30px'
+    }
+
+  },
+  plugins: [],
+}
+
